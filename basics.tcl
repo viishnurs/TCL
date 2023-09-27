fd66494d04e8c8c677bb1d83c52987f633e9a34d
vishnu rs <85409531+viishnurs@users.noreply.github.com>
2023-09-27 21:34:05 +0530

    Create basics.tcl

---
+++ basics.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/tclsh
+
+# --->> we can use '#' this for commants 
+# set : set is used to assign the variable with elements , elements can be of any type(char ,int ,string)
+
+set a 10
+set b {12 21 312}
+
+puts "content in a is $a"
+#if we use " " if prints the contant of a 
+puts {we use contant and $a is treares as normal}
+puts $b     
+# we us $ to get the contant which we assigned  if we have to add string along with contant we assgined we use " ".
